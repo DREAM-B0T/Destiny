eec1590d21f77cca75262355fcc232fe0acadb62
DREAM-B0T <95890009+DREAM-B0T@users.noreply.github.com>
2022-01-19 20:56:34 -0500

    Update index.js

--- index.js
+++ index.js
@@ -829,7 +829,7 @@ m.reply(gps)
 break 
 
 	case 'lista':
-	const { lista } = requiere ('./functions/lista');
+	const { lista } = requiere('./functions/lista');
 		Fg.sendMessage(from, desthumb, image, {thumbnail: fakethumb, quoted: mek, cation: lista})
 	break
 		
