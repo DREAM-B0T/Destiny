87bc4a90c402afd0af5d58f18fa025dc69500705
DREAM-B0T <95890009+DREAM-B0T@users.noreply.github.com>
2022-01-19 20:50:32 -0500

    Update index.js

--- index.js
+++ index.js
@@ -829,6 +829,8 @@ m.reply(gps)
 break 
 
 	case 'lista':
+		const 'lista' = ('./functions/lista');
+		
 		Fg.sendMessage(from, desthumb, image, {thumbnail: fakethumb, quoted: mek, cation: lista})
 	break
 		
